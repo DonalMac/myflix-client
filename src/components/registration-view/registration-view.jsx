1fcc9694114b3bbd54d09e363336b66937e12892
DonalMac <98590507+DonalMac@users.noreply.github.com>
2022-04-06 23:10:05 +0200

    Login & Registration 3.4

---
+++ src/components/registration-view/registration-view.jsx
@@ -0,0 +1,29 @@
+import react, { useState } from "react";
+
+import './login-view.scss';
+export function RegistrationView(props) {
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, password);
+    /* Send a request to the server for authentication */
+    /* then call props.onLoggedIn(name) */
+    props.onLoggedIn(name);
+  };
+
+  return (
+    <form>
+      <label>
+        Name:
+        <input type="text" value={name} onChange={e => setName(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      </label>
+      <button type="submit" onClick={handleSubmit}>Submit</button>
+    </form>
+  );
+}
