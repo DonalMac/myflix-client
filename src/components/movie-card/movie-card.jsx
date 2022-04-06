8890c2a5b8f61f674f6644ada67d94a60a56e20a
DonalMac <98590507+DonalMac@users.noreply.github.com>
2022-04-06 23:01:41 +0200

    3.4 side branch updates

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import './movie-card.scss';
 
 export class MovieCard extends React.Component {
   render() {
     const { movie, onMovieClick } = this.props;
-
     return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired
+  }).isRequired,
+  index: PropTypes.number.isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import './movie-view.scss';
+
 
 export class MovieView extends React.Component {
+  keypressCallback(event) {
+    console.log(event.key);
+  }
+  componentDidMount() {
+    document.addEventListener('keypress', this.keypressCallback);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keypress', this.keypressCallback);
+  }
 
   render() {
     const { movie, onBackClick } = this.props;
@@ -24,3 +38,13 @@ export class MovieView extends React.Component {
     );
   }
 }
+
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired
+  }).isRequired,
+  index: PropTypes.number.isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
