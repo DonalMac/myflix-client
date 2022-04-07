7d05451f742561c333a10e541067025865af48b2
DonalMac <98590507+DonalMac@users.noreply.github.com>
2022-04-07 20:15:07 +0200

    Bootstrap 3.5

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 import './login-view.scss';
 
@@ -16,16 +18,19 @@ export function LoginView(props) {
   };
 
   return (
-    <form>
-      <label>
-        Name:
-        <input type="text" value={name} onChange={e => setName(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-      </label>
-      <button type="submit" onClick={handleSubmit}>Submit</button>
-    </form>
+    <Form>
+      <Form.Group controlId="formUsername">
+        <Form.Label>Name:</Form.Label>
+        <Form.Control type="text" onChange={e => setName(e.target.value)} />
+      </Form.Group>
+
+      <Form.Group controlId="formPassword">
+        <Form.Label>Password:</Form.Label>
+        <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
+      </Form.Group>
+      <Button variant="primary" type="submit" onClick={handleSubmit}>
+        Submit
+      </Button>
+    </Form>
   );
 }
